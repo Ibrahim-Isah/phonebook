b557d3f559a99019445629729931611a3f081574
Isah Ibrahim <isahibn08@gmail.com>
2023-12-06 12:43:53 +0100

    added atlas

--- models/Person.js
+++ models/Person.js
@@ -1,37 +1,37 @@
 const mongoose = require('mongoose');
 
 mongoose
-// eslint-disable-next-line no-undef
-  .connect(process.env.MONGODB_LOCAL)
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
+	// eslint-disable-next-line no-undef
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log('connected to MongoDB');
+	})
 
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message);
+	});
 
 const Schema = mongoose.Schema;
 
 const personSchema = new Schema({
-  name: {
-    required: true,
-    type: String,
-    min: 3,
-  },
-  number: {
-    required: true,
-    type: String,
-    min: 8,
-  },
+	name: {
+		required: true,
+		type: String,
+		min: 3,
+	},
+	number: {
+		required: true,
+		type: String,
+		min: 8,
+	},
 });
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
 });
 
 module.exports = mongoose.model('Person', personSchema);
